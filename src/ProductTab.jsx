5424b1bd4384a82fbdb288abdf43c2a9f83e53ad
angcon175 <angcon26011998@gmail.com>
2024-09-22 11:00:55 +0600

    css

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,12 +1,13 @@
 import Product from "./Product.jsx";
+import "./ProductTab.css";
 function ProductTab() {
   return (
-    <>
+    <div className="style">
       <Product title="Logitech Mx Master" idx={0}/>
       <Product title="Apple Pencil(2nd Gen)" idx={1}/>
       <Product title="Zebronics Zeb-transformer" idx= {2}/>
       <Product title="Petronics Toad 23" idx={3}/>
-    </>
+    </div>
   );
 }
 
