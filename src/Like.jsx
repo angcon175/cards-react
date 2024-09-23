3456b92316013b7081d4541349861b4316dad0d9
angcon175 <angcon26011998@gmail.com>
2024-09-23 17:22:33 +0600

    click count with like

--- src/Like.jsx
+++ src/Like.jsx
@@ -2,13 +2,16 @@ import { useState } from "react";
 
 export default function Like() {
     let [isLiked,setIsLiked] = useState(false)
+    let [click, setClick] = useState(0)
 
     let changeLike = () => {
         setIsLiked(!isLiked)
+        setClick(click+1)
     }
 
     return (
         <>
+            <p onClick={changeLike}>Click = {click}</p>
             <p onClick={changeLike}>
                 {
                     isLiked ? (<i className="fa-solid fa-heart"></i>) : (<i className="fa-regular fa-heart"></i>)
